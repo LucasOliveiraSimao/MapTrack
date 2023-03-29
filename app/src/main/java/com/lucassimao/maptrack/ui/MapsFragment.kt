bf25ac13d4f387137648215e24615346c32997f5
Lucas <oliveira.lucas20@gmail.com>
2023-03-29 16:55:23 -0400

    feat: Refactored permission checking using EasyPermission library
    Refactored the permission checking logic to use the EasyPermission library for better code readability and maintainability.
    
    feat: Added function to retrieve and mark user's current location on map
    
    Implemented a new function that retrieves the user's current location and displays it on the map with a marker. This improves the user experience by allowing them to easily see their current location within the app.

--- app/src/main/java/com/lucassimao/maptrack/ui/MapsFragment.kt
+++ app/src/main/java/com/lucassimao/maptrack/ui/MapsFragment.kt
@@ -1,20 +1,26 @@
 package com.lucassimao.maptrack.ui
 
-import androidx.fragment.app.Fragment
-
+import android.Manifest
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.lucassimao.maptrack.R
 import com.lucassimao.maptrack.databinding.FragmentMapsBinding
 import com.lucassimao.maptrack.util.Constants.GOOGLE_MAPS_CAMERA_ZOOM_VALUE
-import com.lucassimao.maptrack.util.checkPermissions
+import com.lucassimao.maptrack.util.Constants.PERMISSION_REQUEST_CODE
+import com.lucassimao.maptrack.util.PermissionHelper.hasLocationPermissions
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(), EasyPermissions.PermissionCallbacks {
     private lateinit var binding: FragmentMapsBinding
 
     private var map: GoogleMap? = null
@@ -25,23 +31,61 @@ class MapsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMapsBinding.inflate(layoutInflater)
+
+        requestPermissions()
         binding.mapView.onCreate(savedInstanceState)
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val city = LatLng(-3.142157912706272, -58.44635731534551)
+        getCurrentLocation()
 
-        requireActivity().checkPermissions()
+    }
 
-        binding.mapView.getMapAsync {
-            map = it
-            it.mapType = GoogleMap.MAP_TYPE_SATELLITE
-            it.moveCamera(CameraUpdateFactory.newLatLngZoom(city, GOOGLE_MAPS_CAMERA_ZOOM_VALUE))
+    private fun requestPermissions() {
+        if (hasLocationPermissions(requireContext())) {
+            return
         }
+        EasyPermissions.requestPermissions(
+            this,
+            getString(R.string.you_need_to_accept_location_permissions_to_use_this_app),
+            PERMISSION_REQUEST_CODE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+        )
 
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getCurrentLocation() {
+
+        val fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireActivity())
+
+        if (hasLocationPermissions(requireContext())) {
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+
+                val cityLatLng = LatLng(location.latitude, location.longitude)
+
+                binding.mapView.getMapAsync { map ->
+                    with(map) {
+                        addMarker(MarkerOptions().position(cityLatLng))
+                        mapType = GoogleMap.MAP_TYPE_SATELLITE
+                        moveCamera(
+                            CameraUpdateFactory.newLatLngZoom(
+                                cityLatLng,
+                                GOOGLE_MAPS_CAMERA_ZOOM_VALUE
+                            )
+                        )
+
+                    }
+
+                }
+            }
+        }
     }
 
     override fun onResume() {
@@ -72,6 +116,25 @@ class MapsFragment : Fragment() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         binding.mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermissions()
+        }
     }
 
 }
--- app/src/main/java/com/lucassimao/maptrack/util/PermissionHelper.kt
+++ app/src/main/java/com/lucassimao/maptrack/util/PermissionHelper.kt
@@ -1,41 +1,13 @@
 package com.lucassimao.maptrack.util
 
 import android.Manifest
-import android.app.Activity
-import android.content.pm.PackageManager
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.lucassimao.maptrack.util.Constants.PERMISSION_REQUEST_CODE
+import android.content.Context
+import pub.devrel.easypermissions.EasyPermissions
 
-fun Activity.checkPermissions(): Boolean {
-
-    val fineLocationPermission = ContextCompat.checkSelfPermission(
-        applicationContext,
-        Manifest.permission.ACCESS_FINE_LOCATION
-    )
-    val coarseLocationPermission = ContextCompat.checkSelfPermission(
-        applicationContext,
+object PermissionHelper {
+    fun hasLocationPermissions(context: Context) = EasyPermissions.hasPermissions(
+        context,
+        Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.ACCESS_COARSE_LOCATION
     )
-
-    val permissionsToRequest = mutableListOf<String>()
-
-    if (fineLocationPermission != PackageManager.PERMISSION_GRANTED) {
-        permissionsToRequest.add(Manifest.permission.ACCESS_FINE_LOCATION)
-    }
-    if (coarseLocationPermission != PackageManager.PERMISSION_GRANTED) {
-        permissionsToRequest.add(Manifest.permission.ACCESS_COARSE_LOCATION)
-    }
-
-    if (permissionsToRequest.isNotEmpty()) {
-        ActivityCompat.requestPermissions(
-            this,
-            permissionsToRequest.toTypedArray(),
-            PERMISSION_REQUEST_CODE
-        )
-        return false
-    }
-
-    return true
-
 }
