d5aa9f5ce0f007b38d8f35b937d2dc291dfcaa30
Lucas <oliveira.lucas20@gmail.com>
2023-04-27 02:43:52 -0400

    feat: adds the POST_NOTIFICATIONS permission for versions equal to or greater than android 13. feat: creates a function that creates a notification, with pause or restart actions, display a timer. feat: creates function that goes to maps screen when click on notification.

--- app/src/main/java/com/lucassimao/maptrack/ui/MainActivity.kt
+++ app/src/main/java/com/lucassimao/maptrack/ui/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.lucassimao.maptrack.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.lucassimao.maptrack.R
 import com.lucassimao.maptrack.databinding.ActivityMainBinding
+import com.lucassimao.maptrack.util.Constants.TRACKING_FRAGMENT_ACTION
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,6 +17,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navigateToMapsFragment(intent)
+
+    }
+
+    private fun navigateToMapsFragment(intent: Intent?) {
+        if (intent?.action == TRACKING_FRAGMENT_ACTION) {
+            val navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            navHostFragment.findNavController().navigate(R.id.action_global_mapsFragment)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToMapsFragment(intent)
     }
 
 }
