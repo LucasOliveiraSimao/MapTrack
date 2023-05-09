e60167c87379d81a2b418f6e4c2cdbd68aa9a63d
Lucas <oliveira.lucas20@gmail.com>
2023-05-09 01:22:11 -0400

    Adds database query operations

---
+++ app/src/main/java/com/lucassimao/maptrack/data/db/MapTrackDAO.kt
@@ -0,0 +1,24 @@
+package com.lucassimao.maptrack.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.lucassimao.maptrack.data.RouteEntity
+
+@Dao
+interface MapTrackDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRoute(route: RouteEntity)
+
+    @Query("SELECT * FROM route_table ORDER BY id DESC")
+    fun getAllRoutes(): LiveData<List<RouteEntity>>
+
+    @Query("SELECT SUM(distanceTraveledInKM) FROM route_table")
+    fun getTotalDistance(): LiveData<Float>
+
+    @Query("SELECT SUM(totalExecutionTime) FROM route_table")
+    fun getTotalExecutionTime(): LiveData<Long>
+}
