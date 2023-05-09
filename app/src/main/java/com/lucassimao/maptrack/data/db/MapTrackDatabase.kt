5389b28f576b6447ae26215e7df5e9a62141b943
Lucas <oliveira.lucas20@gmail.com>
2023-05-09 01:11:52 -0400

    Creates database

---
+++ app/src/main/java/com/lucassimao/maptrack/data/db/MapTrackDatabase.kt
@@ -0,0 +1,17 @@
+package com.lucassimao.maptrack.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.lucassimao.maptrack.data.RouteEntity
+import com.lucassimao.maptrack.util.ConverterBitmapUtil
+
+@Database(
+    entities = [RouteEntity::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(ConverterBitmapUtil::class)
+abstract class MapTrackDatabase : RoomDatabase() {
+    abstract fun mapTrackDAO(): MapTrackDAO
+}
