299903b326cdb84c28e68d803f7d520368f720da
Lucas <oliveira.lucas20@gmail.com>
2023-05-09 01:16:38 -0400

    Creates function to convert milliseconds to hours and format date

--- app/src/main/java/com/lucassimao/maptrack/util/TimeUtil.kt
+++ app/src/main/java/com/lucassimao/maptrack/util/TimeUtil.kt
@@ -1,5 +1,11 @@
 package com.lucassimao.maptrack.util
 
+import com.lucassimao.maptrack.util.Constants.MILLISECONDS_TO_SECONDS_CONVERSION_FACTOR
+import com.lucassimao.maptrack.util.Constants.MINUTES_TO_HOURS_CONVERSION_FACTOR
+import com.lucassimao.maptrack.util.Constants.SECONDS_TO_MINUTES_CONVERSION_FACTOR
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 import java.util.concurrent.TimeUnit
 
 fun calculateElapsedTime(initialTime: Long): Long {
@@ -18,4 +24,17 @@ fun getFormattedElapsedTime(totalTimeInMilliseconds: Long): String {
     return "${if (hour < 10) "0" else ""}$hour:" +
             "${if (minutes < 10) "0" else ""}$minutes:" +
             "${if (seconds < 10) "0" else ""}$seconds"
+}
+
+fun millisToHours(totalTimeInMilliseconds: Long): Float {
+    return totalTimeInMilliseconds /
+            MILLISECONDS_TO_SECONDS_CONVERSION_FACTOR /
+            SECONDS_TO_MINUTES_CONVERSION_FACTOR /
+            MINUTES_TO_HOURS_CONVERSION_FACTOR
+}
+
+fun formatDate(timeInMillis: Long): String {
+    val pattern = "dd/MM/yy"
+    val simpleDateFormat = SimpleDateFormat(pattern, Locale.getDefault())
+    return simpleDateFormat.format(Date(timeInMillis))
 }
