b48433bf153040d62fc636915529c008736fd499
Lucas de Oliveira Simão <Oliveira.lucas20@gmail.com>
2023-12-10 21:13:59 -0400

    Refactors function and test

--- app/src/test/java/com/lucassimao/maptrack/data/entity/SpeedCalculatorEntityTest.kt
+++ app/src/test/java/com/lucassimao/maptrack/data/entity/SpeedCalculatorEntityTest.kt
@@ -1,7 +1,6 @@
 package com.lucassimao.maptrack.data.entity
 
 import com.lucassimao.maptrack.base.SpeedAverageBase.distanceInMeters
-import com.lucassimao.maptrack.base.SpeedAverageBase.expected
 import com.lucassimao.maptrack.base.SpeedAverageBase.totalTimeInMilliseconds
 import io.mockk.every
 import io.mockk.mockk
@@ -12,10 +11,9 @@ class SpeedCalculatorEntityTest {
     @Test
     fun `calculate average speed`() {
 
-        val mockConverterMetersToKilometers = mockk<(Float) -> Float>()
         val mockConverterMillisToHours = mockk<(Long) -> Float>()
+        val expected = 500.0f
 
-        every { mockConverterMetersToKilometers.invoke(any()) } returns expected
         every { mockConverterMillisToHours.invoke(any()) } returns 1.0f
 
         val result = SpeedCalculatorEntity().calculateAverageSpeed(
