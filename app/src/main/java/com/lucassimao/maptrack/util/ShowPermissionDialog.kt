75c3fa9a9f7459d8bfdbff95073af8f332059bde
Lucas de Oliveira Simão <Oliveira.lucas20@gmail.com>
2023-12-13 13:34:12 -0400

    Refactors the function that creates the alert dialog, separating the responsibilities

--- app/src/main/java/com/lucassimao/maptrack/util/ShowPermissionDialog.kt
+++ app/src/main/java/com/lucassimao/maptrack/util/ShowPermissionDialog.kt
@@ -1,33 +1,51 @@
 package com.lucassimao.maptrack.util
 
+import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
-import androidx.appcompat.app.AlertDialog
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.lucassimao.maptrack.R
 
-fun Fragment.showPermissionDialog(
+fun Fragment.showAlertDialog(
     positiveAction: () -> Unit,
     negativeAction: () -> Unit,
-    title: String = getString(R.string.permission_required),
+    title: String? = null,
     messageLayoutId: Int = R.layout.custom_justified_text,
-    positiveButtonText: String = "Aceito",
-    negativeButtonText: String = "Não Aceito"
+    message: String? = null,
+    positiveButtonText: String? = null,
+    negativeButtonText: String? = null
 ) {
-    val inflater = LayoutInflater.from(this.requireContext())
-    val messageView: View = inflater.inflate(messageLayoutId, null)
+    val alertDialogView = createAlertDialogView(messageLayoutId, title, message)
 
+    val alertBuilder = buildAlertDialog(alertDialogView)
 
-    val alertBuilder = AlertDialog.Builder(this.requireContext())
-    alertBuilder.setCancelable(false)
-    alertBuilder.setTitle(title)
-    alertBuilder.setView(messageView)
-    alertBuilder.setPositiveButton(positiveButtonText) { _, _ ->
-        positiveAction()
-    }
-    alertBuilder.setNegativeButton(negativeButtonText) { _, _ ->
-        negativeAction()
-    }
-    alertBuilder.create()
+    configurePositiveButton(alertBuilder, positiveButtonText, positiveAction)
+    configureNegativeButton(alertBuilder, negativeButtonText, negativeAction)
+
     alertBuilder.show()
+}
+
+private fun Fragment.createAlertDialogView(layoutId: Int, title: String?, message: String?): View {
+    val inflater = LayoutInflater.from(requireContext())
+    val alertDialogView = inflater.inflate(layoutId, null)
+
+    alertDialogView.findViewById<TextView>(R.id.alert_title)?.text = title
+    alertDialogView.findViewById<TextView>(R.id.alert_message)?.text = message
+
+    return alertDialogView
+}
+
+private fun Fragment.buildAlertDialog(view: View): AlertDialog.Builder {
+    return AlertDialog.Builder(requireContext())
+        .setCancelable(false)
+        .setView(view)
+}
+
+private fun configurePositiveButton(builder: AlertDialog.Builder, buttonText: String?, action: () -> Unit) {
+    builder.setPositiveButton(buttonText) { _, _ -> action() }
+}
+
+private fun configureNegativeButton(builder: AlertDialog.Builder, buttonText: String?, action: () -> Unit) {
+    builder.setNegativeButton(buttonText) { _, _ -> action() }
 }
