aa13bba9a104757d6b48ffe9b422eec5c30e7f63
Lucas de Oliveira Simão <Oliveira.lucas20@gmail.com>
2023-12-24 20:39:04 -0400

    Moves to the package

--- app/src/main/java/com/lucassimao/maptrack/commom/FormatFloatToTwoDecimalPlaces.kt
+++ app/src/main/java/com/lucassimao/maptrack/commom/FormatFloatToTwoDecimalPlaces.kt
@@ -1,4 +1,4 @@
-package com.lucassimao.maptrack.data.core
+package com.lucassimao.maptrack.commom
 
 fun formatFloatToTwoDecimalPlaces(value: Float): String {
     return String.format("%.2f",value)
